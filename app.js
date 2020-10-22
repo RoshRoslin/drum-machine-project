2ce01786e3a577762f48ad3486b6f94c41b50789
RoshRoslin <RoshRoslin@gmail.com>
2020-10-21 18:40:08 -0700

    added click event for kick.

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 const keys = document.querySelectorAll('.key');
+const kick = document.getElementById('kick');
+const snare = document.getElementById('snare');
+const hiHat = document.getElementById('hi-hat');
+const clap = document.getElementById('clap');
+const tom1 = document.getElementById('tom1');
+const crash = document.getElementById('crash');
+
 
 window.addEventListener('keydown', function(e){
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
@@ -16,10 +23,29 @@ window.addEventListener('keydown', function(e){
   //play sample
   audio.play();
 
-
-
 });
 
+kick.addEventListener('click', function(e){
+  const kickAudio = document.querySelector(`audio[data-key="65"]`);
+  const kickSampleKey = document.querySelector(`.key[data-key="65"]`);
+
+  if(!kickAudio){
+    return
+  }
+
+
+  kickSampleKey.classList.add('playing');
+
+  //rewind sample
+  kickAudio.currentTime = 0;
+
+  //play sample
+  kickAudio.play();
+
+})
+
+
+// add event listener for each key press to remove transition
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 
 function removeTransition(e){
