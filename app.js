3ba9b58a735597e6dbe4f8ac7969e47197cb9939
RoshRoslin <RoshRoslin@gmail.com>
2020-10-22 13:47:07 -0700

    2 drummers. Fix the keydown for Rosh

--- app.js
+++ app.js
@@ -7,30 +7,51 @@ const tom1 = document.getElementById('tom1');
 const crash = document.getElementById('crash');
 const eightOhEight = document.getElementById('eight-oh-eight');
 const eightOhEightDrummer = document.getElementById('eight-oh-eight-drummer');
-// const rosh = document.getElementById('rosh');
-// const roshDrummer= document.getElementById('rosh-drummer');
+const rosh = document.getElementById('rosh');
+const roshDrummer= document.getElementById('rosh__drummer');
+const pickYourDrummer = document.querySelectorAll('.drummer');
+const roshKick = document.getElementById('rosh__kick');
+const roshSnare = document.getElementById('rosh__snare');
+const roshHiHat = document.getElementById('rosh__hi-hat');
+const roshClap = document.getElementById('rosh__clap');
+const roshTom1 = document.getElementById('rosh__tom1');
+const roshCrash = document.getElementById('rosh__crash');
 
+
+
+// select which drummer from menu
+eightOhEight.addEventListener('click', function(e){
+pickYourDrummerHandler();
+window.removeEventListener('keydown', playAudioOnKeyEvent);
+eightOhEightDrummer.style.display = 'block';
 
 //add event listeners
-
+window.addEventListener('keydown', playAudioOnKeyEvent);
 kick.addEventListener('click', playAudioOnClick);
 snare.addEventListener('click', playAudioOnClick);
 hiHat.addEventListener('click', playAudioOnClick);
 clap.addEventListener('click', playAudioOnClick);
 tom1.addEventListener('click', playAudioOnClick);
 crash.addEventListener('click', playAudioOnClick);
-window.addEventListener('keydown', playAudioOnKeyEvent);
-
-
-// select which drummer from menu
-eightOhEight.addEventListener('click', function(e){
-eightOhEightDrummer.style.display = 'block';
 });
 
-// rosh.addEventListener('click', function(e){
-//
-// roshDrummer.style.display = 'block';
-// });
+
+
+rosh.addEventListener('click', function(e){
+pickYourDrummerHandler();
+
+//add event listeners
+roshDrummer.style.display = 'block';
+window.removeEventListener('keydown', playAudioOnKeyEvent);
+
+window.addEventListener('keydown', playAudioOnKeyEvent);
+roshKick.addEventListener('click', playAudioOnClick);
+roshSnare.addEventListener('click', playAudioOnClick);
+roshHiHat.addEventListener('click', playAudioOnClick);
+roshClap.addEventListener('click', playAudioOnClick);
+roshTom1.addEventListener('click', playAudioOnClick);
+roshCrash.addEventListener('click', playAudioOnClick);
+});
 
 
 
@@ -63,6 +84,7 @@ function playAudioOnClick(e){
   const audio = document.querySelector(`audio[data-key="${dataId}"]`);
   const sampleKey = document.querySelector(`.key[data-key="${dataId}"]`);
 
+
   if(!audio){
     return
   }
@@ -86,4 +108,11 @@ function removeTransition(e){
 
   this.classList.remove('playing');
 
+}
+
+//pick your drummer pickYourDrummerHandler
+function pickYourDrummerHandler(){
+  pickYourDrummer.forEach(function(element){
+    element.style.display = 'none';
+  });
 }
